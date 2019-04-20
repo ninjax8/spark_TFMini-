9c3f896c5f8b2cc64fd6f92a4c94965003ae7c03
ninjax8 <mail.subhadeepbiswas@gmail.com>
2019-04-21 00:24:56 +0530

    first commit

--- test1.ino
+++ test1.ino
@@ -1,1 +1,25 @@
-Hello
+#include <TFMini-Public.h>
+
+TFMini tfmini;
+
+void setup() {
+    Serial.begin(115200);
+    Serial1.begin(115200);
+    delay(200);
+    tfmini.begin(&Serial1);
+}
+
+void loop() {
+    
+  uint16_t dist = tfmini.getDistance();
+  uint16_t strength = tfmini.getRecentSignalStrength();
+
+  // Display the measurement
+  Serial.print(dist);
+  Serial.print(" cm      sigstr: ");
+  Serial.println(strength);
+
+  // Wait some short time before taking the next measurement
+  delay(25);
+
+}
